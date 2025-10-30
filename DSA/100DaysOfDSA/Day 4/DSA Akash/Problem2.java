a19e36d64aadc47192fa3613a6d7408b2028250a
Amit <amitkumaramitkumar15351@gmail.com>
2025-10-30 13:09:13 +0530

    Neither Minimum nor Maximum

--- DSA/100DaysOfDSA/Day 4/DSA Akash/Problem2.java
+++ DSA/100DaysOfDSA/Day 4/DSA Akash/Problem2.java
@@ -5,7 +5,7 @@
  */
 public class Problem2 {
     public void reverseString(char[] s) {
-        //two pointer
+        //two pointer approach n/2 tc O(n)
         int start = 0, end = s.length - 1;
         while (start < end) {
             char temp = s[start];
