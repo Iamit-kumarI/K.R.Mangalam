d562a0dc28186b301cc39fa8f2fc7608d643d5fb
Amit <amitkumaramitkumar15351@gmail.com>
2025-10-30 13:08:28 +0530

    [200~Reverse String~

---
+++ DSA/100DaysOfDSA/Day 4/DSA Akash/Problem2.java
@@ -0,0 +1,16 @@
+// package DSA Akash;
+/*
+ * Problem:344. Reverse String
+ * Solution: https://leetcode.com/problems/reverse-string/solutions/7312855/java-two-pointer-beats-100-by-zerogrind-3qsj
+ */
+public class Problem2 {
+    public void reverseString(char[] s) {
+        //two pointer
+        int start = 0, end = s.length - 1;
+        while (start < end) {
+            char temp = s[start];
+            s[start++] = s[end];
+            s[end--] = temp;
+        }
+    }
+}
