c8bcdd4b77479c92c918b1103c84091992458d0e
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-02 00:46:06 +0530

    Equilibrium Point Day 6, 2 Nov 2025

--- DSA/100DaysOfDSA/Day 6/CP/Problem2.java
+++ DSA/100DaysOfDSA/Day 6/CP/Problem2.java
@@ -1,9 +1,11 @@
 package CP;
+
 /*
- * Problem:Equilibrium Point
+ * Problem:Equilibrium Point (gfg)
  * Solution: https://discuss.geeksforgeeks.org/comment/3af0da29-652c-4caa-bdb0-750d09850f12/practice
  */
 public class Problem2 {
+
     public static int findEquilibrium(int arr[]) {
         // code here
         int sum = 0, leftsum = 0;
