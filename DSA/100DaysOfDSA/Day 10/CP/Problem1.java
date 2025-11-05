860e8403e156cad094b1012fc719fb709fe07857
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-06 03:55:47 +0530

    Monotonic Array

---
+++ DSA/100DaysOfDSA/Day 10/CP/Problem1.java
@@ -0,0 +1,47 @@
+package CP;
+/*
+ * Problem:896. Monotonic Array
+ * Solution: https://leetcode.com/problems/monotonic-array/solutions/7329097/java-loop-beats-100-by-amitk300-3o9c
+ */
+public class Problem1 {
+    public boolean isMonotonic(int[] nums) {
+        if (nums.length == 1) {
+            return true;
+        }
+        // if(nums[0]<nums[1])return inccheck(nums);
+        // else return descheck(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (i == nums.length - 1) {
+                return true;
+            }
+            if (nums[i - 1] == nums[i]) {
+                continue;
+            }
+            if (nums[i - 1] < nums[i]) {
+                return inccheck(nums);
+            }
+            if (nums[i - 1] > nums[i]) {
+                return descheck(nums);
+            }
+        }
+        return false;
+    }
+
+    public boolean inccheck(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean descheck(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > arr[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
