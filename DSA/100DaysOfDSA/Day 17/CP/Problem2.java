985abca826ef5a084ebe01bb1627a536b2e66b42
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-13 14:19:46 +0530

    Bubble Sort Day 17, 13 Nov 2025

--- DSA/100DaysOfDSA/Day 17/CP/Problem2.java
+++ DSA/100DaysOfDSA/Day 17/CP/Problem2.java
@@ -1,3 +1,19 @@
+/*
+ * Problem:Bubble Sort
+ * Solution:https://www.geeksforgeeks.org/problems/bubble-sort/1
+ */
 public class Problem2 {
-    
+    public void bubbleSort(int[] arr) {
+        // code here
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
 }
