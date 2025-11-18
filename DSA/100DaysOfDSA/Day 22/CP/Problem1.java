b0aceb35c65559dc0cd9a154542b8486bb40db7d
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-18 13:25:53 +0530

    Merge Sorted Array, Day 22, 18 Nov 2025

---
+++ DSA/100DaysOfDSA/Day 22/CP/Problem1.java
@@ -0,0 +1,23 @@
+/*
+ * Problem:88. Merge Sorted Array
+ * Solution:https://leetcode.com/problems/merge-sorted-array/solutions/7355902/java-loop-index-beats-100-by-zerogrind-vicr
+ * 
+ */
+public class Problem1 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len = n + m - 1;
+        // int max=0;
+        m -= 1;
+        n -= 1;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] >= nums2[n]) {
+                nums1[len--] = nums1[m--]; 
+            }else {
+                nums1[len--] = nums2[n--];
+            }
+        }
+        while (n >= 0) {
+            nums1[len--] = nums2[n--];
+        }
+    }
+}
