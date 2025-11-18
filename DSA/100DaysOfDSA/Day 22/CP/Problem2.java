de238ac55d449de387cc90ad3b5808b452dda1b0
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-18 13:31:52 +0530

    33. Search in Rotated Sorted Array, Day 22, 18 Nov 2025

---
+++ DSA/100DaysOfDSA/Day 22/CP/Problem2.java
@@ -0,0 +1,30 @@
+/*
+ * Problem:33. Search in Rotated Sorted Array
+ * Solution:https://leetcode.com/problems/search-in-rotated-sorted-array/solutions/7342298/java-recursion-beats-100-by-zerogrind-tm4z
+ */
+public class Problem2 {
+    public int search(int[] nums, int target) {
+        return rotatedSearch(nums, target, 0, nums.length - 1);
+    }
+
+    public int rotatedSearch(int[] nums, int target, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = start + (end - start) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        }
+        if (nums[start] <= nums[mid]) {
+            if (target >= nums[start] && target <= nums[mid]) {
+                return rotatedSearch(nums, target, start, mid - 1);
+            } else {
+                return rotatedSearch(nums, target, mid + 1, end);
+            }
+        }
+        if (target >= nums[mid] && target <= nums[end]) {
+            return rotatedSearch(nums, target, mid + 1, end);
+        }
+        return rotatedSearch(nums, target, start, mid - 1);
+    }
+}
