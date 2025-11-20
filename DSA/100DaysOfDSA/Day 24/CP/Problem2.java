0c9b820e380aab7a5024eadf1a4e1aa81de1528b
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-20 14:23:23 +0530

    Stable Sorting Algorithm(merg sort) Day 24, 20 Nov 2025

---
+++ DSA/100DaysOfDSA/Day 24/CP/Problem2.java
@@ -0,0 +1,47 @@
+/*
+* Problem:Stable Sort Concept (practical)(Merge Sort)
+* Solution:
+* Statble Sort: order of element dosent change after sorting(Bubble Sort, Insertion Sort, Merge Sort, Count Sort)
+* Not Stable: order of element may change after sortingSelection Sort, Quick Sort, Heap Sort
+*/
+import java.util.Arrays;
+public class Problem2 {
+    void mergeSort(int arr[], int l, int r) {
+        // code here
+        int[] sortarray = helper(arr, l, r + 1);
+        for (int i = 0; i < sortarray.length; i++) {
+            arr[l + i] = sortarray[i];
+        }
+    }
+
+    public int[] helper(int[] arr, int l, int r) {
+        if (r - l <= 1) {
+            return Arrays.copyOfRange(arr, l, r);
+        }
+        int m = l + (r - l) / 2;
+        int[] left = helper(arr, l, m);
+        int[] right = helper(arr, m, r);
+        return merge(left, right);
+    }
+
+    public int[] merge(int[] left, int[] right) {
+        int i = 0, j = 0, index = 0;
+        int[] sortedarr = new int[left.length + right.length];
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                //put left elem in new sorted array
+                sortedarr[index++] = left[i++];
+            } else {
+                sortedarr[index++] = right[j++];
+            }
+        }
+        //check which array becomes empty
+        while (j < right.length) {
+            sortedarr[index++] = right[j++];
+        }
+        while (i < left.length) {
+            sortedarr[index++] = left[i++];
+        }
+        return sortedarr;
+    }
+}
