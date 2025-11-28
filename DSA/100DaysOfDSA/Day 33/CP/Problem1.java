38754b21275e10111c193c6a98bfb39a9b1272df
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-29 04:03:34 +0530

    Upper Case Conversion, Day 33, 29 Nov 2025

---
+++ DSA/100DaysOfDSA/Day 33/CP/Problem1.java
@@ -0,0 +1,20 @@
+/*
+* Problem:Upper Case Conversion
+* Solution: https://discuss.geeksforgeeks.org/comment/d8af3b9c-3419-47ca-82bf-1c65b60e64b0/practice
+*/
+class Problem1 {
+    public String convert(String s) {
+        // code here
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (i == 0 || s.charAt(i - 1) == ' ') {
+                sb.append(Character.toUpperCase(s.charAt(i)));
+                continue;
+            }
+            sb.append(s.charAt(i));
+        }
+
+        return sb.toString();
+    }
+}
+
