13cf597bec88c7c3e3599425ea8a01e99392d781
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-29 04:05:59 +0530

    Remove Spaces, Day 33, 29 Nov 2025

---
+++ DSA/100DaysOfDSA/Day 33/CP/Problem2.java
@@ -0,0 +1,16 @@
+/*
+* Problem:Remove Spaces
+* Solution:https://discuss.geeksforgeeks.org/comment/600d8843-1c44-44e8-9766-85a207ec8ed6/practice
+*/
+public class Problem2 {
+    String modify(String s) {
+        // your code here
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (c != ' ') {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
