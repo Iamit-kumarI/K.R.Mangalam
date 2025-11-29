f928d3725e14d728c0a7955e3bb7b9098fc849d8
Amit <amitkumaramitkumar15351@gmail.com>
2025-11-29 17:23:30 +0530

    Remove Spaces Day 30, Nov 2025

--- DSA/100DaysOfDSA/Day 33/CP/Problem2.java
+++ DSA/100DaysOfDSA/Day 33/CP/Problem2.java
@@ -2,7 +2,7 @@
 * Problem:Remove Spaces
 * Solution:https://discuss.geeksforgeeks.org/comment/600d8843-1c44-44e8-9766-85a207ec8ed6/practice
 */
-public class Problem2 {
+public class Problem2 {//by mistake i have put 29 nov its 30 nov
     String modify(String s) {
         // your code here
         StringBuilder sb = new StringBuilder();
