a0e4883c2c46bc66a1b8c66e4c47aa76314a9df1
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-19 00:26:43 +0530

    Problem 1 Not Found, Day 54, 19 Dec 2025

---
+++ DSA/100DaysOfDSA/Day 54/CP/Problem1.java
@@ -0,0 +1,4 @@
+public class Problem1 {
+    //again this happening from last 3 consicative days
+    //problem link is not the actual problem link
+}
---
+++ DSA/100DaysOfDSA/Day 54/CP/Problem2.java
@@ -0,0 +1,15 @@
+/*
+* Problem:Matrix Addition (basic)
+* Solution:https://www.geeksforgeeks.org/problems/addition-of-two-square-matrices4916/1
+*/
+// public class Problem2 {
+//     public void Addition(int[][] matrixA, int[][] matrixB) {
+//         // code here
+//         int n = matrixA.length, m = matrixA.length;
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 matrixA[i][j] += matrixB[i][j];
+//             }
+//         }
+//     }
+// }
