632f1234359827c116baf8322bba58e619366d3c
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-20 07:49:50 +0530

    Delete in a Singly Linked List, Day 55, 20 Dec 2025

---
+++ DSA/100DaysOfDSA/Day 55/CP/Problem1.java
@@ -0,0 +1,28 @@
+/*
+* Problem:Delete Node at Position in Linked List
+* Solution:https://discuss.geeksforgeeks.org/comment/732fc498-3f62-46a6-b0f8-d7edddfa1989/practice
+*/
+class Problem1 {
+    Node deleteNode(Node head, int x) {
+        // code here
+        Node thead=head;
+        Node prev=head;
+        if(x==1)return head.next;
+        while(x-->1){//thead!=null no need to check
+            prev=thead;
+            thead=thead.next;
+        }
+        prev.next=thead.next;
+        return head;
+    }
+    class Node {
+
+        int data;
+        Node next;
+
+        Node(int d) {
+            this.data = d;
+            this.next = null;
+        }
+    }
+}
