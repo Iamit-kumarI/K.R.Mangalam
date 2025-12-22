dfdf1cb8c80ca9b05837fdbb822df6d91e35630a
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-22 09:11:36 +0530

    Reverse a linked list, Day 57, 22 Dec 2025

---
+++ DSA/100DaysOfDSA/Day 57/CP/Program2.java
@@ -0,0 +1,24 @@
+package CP;
+/*
+* Problem:Reverse Linked List (iterative)
+* Solution:https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
+*/
+public class Program2 {
+    Node node=new Node();
+    Node reverseList(Node head) {
+        // code here
+        if (head == null) {
+            return head;
+        }
+        Node prev = null, cur = head, next = head.next;
+        while (cur != null) {
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
