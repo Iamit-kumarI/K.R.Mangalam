1549da1b637ed4a5de3455e6c10b535922a69b47
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-25 12:16:10 +0530

    2. Add Two Numbers, Day 58, 23 Dec 2025

---
+++ DSA/100DaysOfDSA/Day 60/CP/Problem1.java
@@ -0,0 +1,52 @@
+/*
+* Problem:Add Two Numbers (Linked List)
+* Solution:https://leetcode.com/problems/add-two-numbers/solutions/7437185/java-linkedlist-beats-100-by-amitk300-0c72
+*/
+package CP;
+class Problem1 {
+    ListNode ListNode=new ListNode();
+    int carry = 0;
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode node = new ListNode(0);
+        ListNode ans = node, thead1 = l1, thead2 = l2;
+        while (thead1 != null && thead2 != null) {
+            int val = thead1.val + thead2.val + carry;
+            node.next = new ListNode(val % 10);
+            carry = val / 10;
+            node = node.next;
+            thead1 = thead1.next;
+            thead2 = thead2.next;
+        }
+        if (thead1 != null) {
+            while (thead1 != null) {
+                int val = thead1.val + carry;
+                node.next = new ListNode(val % 10);
+                carry = val / 10;
+                thead1 = thead1.next;
+                node = node.next;
+            }
+        }
+        if (thead2 != null) {
+            while (thead2 != null) {
+                int val = thead2.val + carry;
+                node.next = new ListNode(val % 10);
+                carry = val / 10;
+                thead2 = thead2.next;
+                node = node.next;
+            }
+        }
+        if (carry != 0) {
+            node.next = new ListNode(carry);
+            node = node.next;
+            node = null;
+        }
+        return ans.next;
+    }
+}
