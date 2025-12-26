67d55bfcca46500de9f1d92c7e0368743b96705c
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-26 19:09:25 +0530

    61. Rotate List, Day 61: 26 Dec  2025

---
+++ DSA/100DaysOfDSA/Day 61/CP/Problem2.java
@@ -0,0 +1,35 @@
+package CP;
+
+/*
+* Problem:Rotate Linked List
+* Solution:https://leetcode.com/problems/rotate-list/solutions/7440907/java-mod-beats-100-by-amitk300-4j0w
+ */
+public class Problem2 {
+
+    ListNode ListNode = new ListNode();
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        ListNode thead = head;
+        int len = 1;
+        while (thead.next != null) {
+            len++;
+            thead = thead.next;
+        }
+        ListNode lastNode = thead;;
+        k %= len;
+        if (k == 0) {
+            return head;
+        }
+        thead = head;
+        for (int i = 1; i < len - k; i++) {
+            thead = thead.next;
+        }
+        ListNode newHead = thead.next;
+        thead.next = null;
+        lastNode.next = head;
+        return newHead;
+    }
+}
