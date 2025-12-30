d500a1cd973c091d9f522badf5467bb07e09269e
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-30 10:34:22 +0530

    237. Delete Node in a Linked List Day 65, 30 Dec 2025

--- DSA/100DaysOfDSA/Day 65/CP/Problem2.java
+++ DSA/100DaysOfDSA/Day 65/CP/Problem2.java
@@ -1,5 +1,5 @@
 /*
-* Problem:Delete Node given only access to that node
+* Problem:Delete Node given only access to that node.
 * Solution:https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/1869004581/
 */
 public class Problem2 {
