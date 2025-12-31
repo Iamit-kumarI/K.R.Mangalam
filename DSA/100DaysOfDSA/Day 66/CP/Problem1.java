21af7905150772b2f9c58e539ca1d984dcd23fef
Amit <amitkumaramitkumar15351@gmail.com>
2025-12-31 12:38:53 +0530

    147. Insertion Sort List Day 66, 31 Dec 2025

--- DSA/100DaysOfDSA/Day 66/CP/Problem1.java
+++ DSA/100DaysOfDSA/Day 66/CP/Problem1.java
@@ -9,7 +9,7 @@ public class Problem1 {
     ListNode ListNode =new ListNode();
     public ListNode insertionSortList(ListNode head) {
         // I don't know how to do this insertion way but here is the  sorting way
-        // we don't have prev value so we can't have track of next next smaller element how can we do that
+        // we don't have prev value so we can't have track of prev prev smaller element how can we do that
         ArrayList<Integer>list=new ArrayList<>();
         ListNode thead=head;
         while(thead!=null){
