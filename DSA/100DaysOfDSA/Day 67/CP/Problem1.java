8f26624f16509c011e8d39450e749e822c1b55ea
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-01 18:07:52 +0530

    25. Reverse Nodes in k-Group, Day 67 1 Jan 2026

---
+++ DSA/100DaysOfDSA/Day 67/CP/Problem1.java
@@ -0,0 +1,35 @@
+/*
+* Problem:Reverse Nodes in k-Group
+* Solution:https://leetcode.com/problems/reverse-nodes-in-k-group/solutions/7455647/java-dfs-beats-100-by-amitk300-h5bc
+*/
+package CP;
+
+import java.util.Stack;
+public class Problem1 {
+    ListNode ListNode=new ListNode();
+    public ListNode reverseKGroup(ListNode head, int k) {
+        Stack<ListNode> stack=new Stack<>();
+        ListNode thead=head;
+        ListNode nhead=new ListNode(0);
+        ListNode ans=nhead;
+        int rev=k;
+        while(thead!=null){
+            ListNode prev=thead;
+            while(rev-->0&&thead!=null){
+                stack.push(thead);
+                thead=thead.next;
+            }
+            if(stack.size()!=k){
+                nhead.next=prev;
+                return ans.next;
+            }
+            rev=k;
+            while(!stack.isEmpty()){
+                nhead.next=stack.pop();
+                nhead=nhead.next;
+            }
+            nhead.next=thead;
+        }
+        return ans.next;
+    }
+}
