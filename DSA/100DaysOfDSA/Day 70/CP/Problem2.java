b7888dbeab5fc6734752a1a7ad3beb77ca7b52c0
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-04 13:40:44 +0530

    237. Delete Node in a Linked List, Day 70 4 jan 2026

---
+++ DSA/100DaysOfDSA/Day 70/CP/Problem2.java
@@ -0,0 +1,12 @@
+package CP;
+/*
+* Problem:Delete Nodes with Given Value
+* Solution:https://leetcode.com/problems/delete-node-in-a-linked-list/submissions/1869004581/
+*/
+public class Problem2 {
+    public void deleteNode(ListNode node) {
+        //we don't had head so its not possible to delete this node but we can delete the node next to to just delete that one
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
