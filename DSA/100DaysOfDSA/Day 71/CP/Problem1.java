a42f897f2b5ab7fd6cb8f358d59ebfe93a3d1c56
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-05 14:26:22 +0530

    141. Linked List Cycle, Day 71 5 Jan 2026

---
+++ DSA/100DaysOfDSA/Day 71/CP/Problem1.java
@@ -0,0 +1,16 @@
+package CP;
+/*
+* Problem:Check if Linked List is Circular
+* Solution:https://leetcode.com/problems/linked-list-cycle/submissions/1875068300/
+*/
+public calss Problem1{
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head,fast=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)return true;
+        }
+        return false;
+    }
+}
