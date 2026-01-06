233ceb192710f58f5a3a576715cc7e845f1c9e5e
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-06 13:24:40 +0530

    Frequency in a Linked List, Day 72, 6 Jan 2026

--- DSA/100DaysOfDSA/Day 72/CP/Problem2.java
+++ DSA/100DaysOfDSA/Day 72/CP/Problem2.java
@@ -1,4 +1,24 @@
+package CP;
 
+import java.util.ArrayList;
+
+/*
+* Problem:Count Occurrences of a Value in Linked List
+* Solution:https://discuss.geeksforgeeks.org/comment/1c84cc9c-f099-49d4-b164-6e7f242df780/practice
+*/
 public class Problem2 {
-    
+    public static int count(Node head, int key) {
+        // code here
+        Node node=new Node();
+        if (head == null) {
+            return 0;
+        }
+        Node thead = head;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (thead != null) {
+            list.add(thead.data);
+            thead = thead.next;
+        }
+        return (int) list.stream().filter(x -> x == key).count();
+    }
 }
