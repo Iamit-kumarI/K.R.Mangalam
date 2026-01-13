37e1670e38ca0325a4da5b7005f3bc9fa39fa8b8
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-13 21:52:37 +0530

    155. Min Stack, Day 80, 13 Jan 2025

---
+++ DSA/100DaysOfDSA/Day 80/CP/Problem2.java
@@ -0,0 +1,47 @@
+package CP;
+
+import java.util.Stack;
+/*
+* Problem:Implement Stack with getMax
+* Solution:https://leetcode.com/problems/min-stack/submissions/1881553068/
+*/
+public class Problem2 {
+    class MinStack {
+    Stack<MStack>minStack=new Stack<MStack>();
+    public MinStack() {
+    }
+    
+    public void push(int val) {
+        if(minStack.isEmpty())minStack.push(new MStack(val,val));
+        else minStack.push(new MStack(val,Math.min(minStack.peek().minValue,val)));
+    }
+    
+    public void pop() {
+        minStack.pop();
+    }
+    
+    public int top() {
+        return minStack.peek().val;
+    }
+    
+    public int getMin() {
+        return minStack.peek().minValue;
+    }
+}
+class MStack{
+    int val;
+    int minValue;
+    public MStack(int val,int minValue){
+        this.val=val;
+        this.minValue=minValue;
+    }
+}
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+}
