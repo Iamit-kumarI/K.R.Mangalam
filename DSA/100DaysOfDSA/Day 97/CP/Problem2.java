13657a2caba4e3210f7b73b0623ea9e5b57439f8
Amit <amitkumaramitkumar15351@gmail.com>
2026-01-30 21:42:29 +0530

    Stream First Non-repeating  Day 97, 30 Jan 2026

--- DSA/100DaysOfDSA/Day 97/CP/Problem2.java
+++ DSA/100DaysOfDSA/Day 97/CP/Problem2.java
@@ -1,3 +1,38 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+/*
+* Problem: Stream First Non-repeating
+* Solution: https://www.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+*/
 public class Problem2 {
-    
+
+    public static void main(String[] args) {
+        String str = "aabc";
+        System.out.println(firstNonRepeating(str));
+    }
+
+    public static String firstNonRepeating(String s) {
+        int n = s.length();
+        if (n == 0) {
+            return "";
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        Queue<Character> queue = new LinkedList<>();
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char cur = s.charAt(i);
+            map.put(cur, map.getOrDefault(cur, 0) + 1);
+            queue.add(cur);
+            while (!queue.isEmpty() && map.get(queue.peek()) > 1) {
+                queue.poll();
+            }
+            if (queue.isEmpty()) {
+                ans.append('#'); 
+            }else {
+                ans.append(queue.peek());
+            }
+        }
+        return ans.toString();
+    }
 }
